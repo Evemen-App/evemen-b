69e8bf376ed29e6da6dbe4f1929b755733166866
youngohene <keskot87@gmail.com>
2025-05-17 14:55:46 +0000

    cors added

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user_route.js";
 import productsRouter from "./routes/product_route.js";
+import cors from "cors";
 
 await mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
+
+app.use(cors());
 
 const port = process.env.PORT || 5000
 
